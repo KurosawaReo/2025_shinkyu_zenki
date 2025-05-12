c393e93716a5bc51a7830750795dcecf4634f519
GM2140067\ydauser01 <na010306odj@gmail.com>
2025-05-12 13:55:27 +0900

    ゲームスタート完成

--- Csinkyu/main.cpp
+++ Csinkyu/main.cpp
@@ -3,6 +3,8 @@
 //classの関数を使うのに必要.
 static GameManager gm;
 
+bool gameStarted = false;
+
 void Init() {
 	gm.Init();
 }
@@ -34,11 +36,23 @@ int WINAPI WinMain(
 	Init();
 	//ESCが押されるまでループ.
 	while (ProcessMessage() == 0 && CheckHitKey(KEY_INPUT_ESCAPE) == 0) {
-		ClearDrawScreen();		 //画面クリア.
-		Update();				 //更新処理.
-		Draw();					 //描画処理.
-		ScreenFlip();			 //表画面へ描画.
-		WaitTimer(WAIT_LOOP_MS); //待機時間(m秒)
+		if (!gameStarted) {
+			// ゲームが開始されていない場合は開始案内を表示
+			DrawFormatString(260, 160, GetColor(255, 255, 255), _T("PUSH SPACE"));
+
+			// スペースキーが押されたらゲーム開始
+			if (CheckHitKey(KEY_INPUT_SPACE)) {
+				gameStarted = true;
+			}
+		}
+		else {
+			// ゲームが開始されている場合は通常の処理を実行
+			ClearDrawScreen(); // 画面クリア
+			Update();          // 更新処理
+			Draw();            // 描画処理
+			ScreenFlip();      // 表画面へ描画
+			WaitTimer(WAIT_LOOP_MS); // 待機時間(m秒)
+		}
 	}
 
 	DxLib_End(); //DXライブラリの終了処理
