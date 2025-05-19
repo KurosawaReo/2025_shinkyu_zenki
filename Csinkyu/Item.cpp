6c939eb4be766a5850d3864f81f7963e995a8102
GM2140067\ydauser01 <na010306odj@gmail.com>
2025-05-19 15:28:43 +0900

    アイテム作成中

---
+++ Csinkyu/Item.cpp
@@ -0,0 +1,27 @@
+/*
+   - Item.cpp -
+   ItemŠÇ—.
+*/
+#include "Item.h"
+
+//‰Šú‰».
+void Item::Init()
+{
+
+}
+//XV.
+void Item::Update()
+{
+	
+}
+//•`‰æ.
+void Item::Draw()
+{
+	
+}
+
+
+void Item::ItemMove()
+{
+	
+}
---
+++ Csinkyu/Item.h
@@ -0,0 +1,13 @@
+
+#pragma once
+
+class Item
+{
+private:
+	
+public:
+	void Init();
+	void Update();
+	void Draw();
+	void ItemMove(); //ƒvƒŒƒCƒ„[ˆÚ“®.
+};
